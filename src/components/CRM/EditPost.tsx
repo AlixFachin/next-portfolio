b8aacc1d0829ff28d54dcf41d4ddbf8daa69b923
Alix Fachin <alix.fachin@me.com>
2023-05-16 07:50:28 +0900

    feat: responsive design for edit post meta data form

--- src/components/CRM/EditPost.tsx
+++ src/components/CRM/EditPost.tsx
@@ -75,19 +75,17 @@ const EditPost: React.FC<EditPostProps> = ({ postId }) => {
     <div className="flex max-w-full flex-col bg-white/80 py-8 px-4 md:w-11/12">
       <form onSubmit={handleSubmit(formSubmitHandler)}>
         {/* Meta Data sub-form */}
-        <div className="mb-2 border-2 border-blue-200 p-4">
-          <h2 className="mb-2xc py-2 font-title text-3xl text-blue-400">
+        <div className="mb-2 grid grid-cols-1 gap-2 border-2 border-blue-200 p-4 md:grid-cols-2 lg:grid-cols-4">
+          <h2 className="mb-2xc py-2 font-title text-3xl text-blue-400 md:col-span-2 lg:col-span-4">
             Meta Data
           </h2>
-          <div className="mb-2 flex flex-col  md:flex-row">
-            <label className="mb-2 md:mr-4" htmlFor="title">
-              Title
-            </label>
-            <input
-              className="border py-2 px-3 text-darkgrey-200 md:flex-grow"
-              {...register("title")}
-            />
-          </div>
+          <label className="mb-2 md:mr-4" htmlFor="title">
+            Title
+          </label>
+          <input
+            className="border py-2 px-3 text-darkgrey-200 lg:col-span-3"
+            {...register("title")}
+          />
           <div className="mb-2 flex items-center py-2">
             <input
               type="checkbox"
@@ -104,76 +102,73 @@ const EditPost: React.FC<EditPostProps> = ({ postId }) => {
             />
             <label htmlFor="draft">Featured</label>
           </div>
-          <div className="mb-2 flex flex-col">
-            <label htmlFor="locale">Locale</label>
-            <select
-              className="border py-2 px-3 text-darkgrey-200"
-              {...register("locale")}
-            >
-              <option value="en">English</option>
-              <option value="ja">日本語</option>
-            </select>
+          <label htmlFor="locale" className="md:mr-4 lg:text-right">
+            Locale
+          </label>
+          <select
+            className="border py-2 px-3 text-darkgrey-200"
+            {...register("locale")}
+          >
+            <option value="en">English</option>
+            <option value="ja">日本語</option>
+          </select>
+          <label htmlFor="slug">Slug</label>
+          <input
+            className="border py-2 px-3 text-darkgrey-200 lg:col-span-3"
+            {...register("slug")}
+          />
+          <label htmlFor="publishedDate">Date</label>
+          <input
+            className="border py-2 px-3 text-darkgrey-200 invalid:text-red"
+            {...register("published")}
+          />
+          <label htmlFor="tagString" className="md:mr-4 lg:text-right">
+            Tags
+          </label>
+          <input
+            className="border py-2 px-3 text-darkgrey-200"
+            {...register("tagString")}
+          />
+          <div className="border-red text-red">
+            {errors && <p> {`${errors.tagString?.message}`} </p>}
           </div>
-          <div className="mb-2 flex flex-col">
-            <label htmlFor="slug">Slug</label>
-            <input
-              className="border py-2 px-3 text-darkgrey-200"
-              {...register("slug")}
-            />
-          </div>
-          <div className="mb-2 flex flex-col">
-            <label htmlFor="publishedDate">Date</label>
-            <input
-              className="border py-2 px-3 text-darkgrey-200 invalid:text-red"
-              {...register("published")}
-            />
-          </div>
-          <div className="mb-2 flex flex-col">
-            <label htmlFor="tagString">Tags</label>
-            <input
-              className="border py-2 px-3 text-darkgrey-200"
-              {...register("tagString")}
-            />
-            <div className="border-red text-red">
-              {errors && <p> {`${errors.tagString?.message}`} </p>}
-            </div>
-          </div>
-          <div className="mb-2 flex flex-col">
-            <label htmlFor="description">Description</label>
-            <textarea
-              className="border py-2 px-3 text-darkgrey-200"
-              rows={2}
-              {...register("description")}
-            />
-          </div>
+          <label htmlFor="description" className="md:col-span-2 lg:col-span-4">
+            Description
+          </label>
+          <textarea
+            className="border py-2 px-3 text-darkgrey-200 md:col-span-2 md:row-span-2 lg:col-span-4"
+            rows={2}
+            {...register("description")}
+          />
+          <div className="mb-2 flex flex-col"></div>
         </div>
         <div className="mb- border-2 border-blue-200 p-4">
           <div className="md-editor mb-2 flex flex-col">
-            <label htmlFor="content">Content</label>
-            <Controller
-              control={control}
-              name="content"
-              render={({ field }) => {
-                return (
-                  <MDEditor
-                    value={field.value}
-                    previewOptions={{
-                      rehypePlugins: [[rehypeSanitize]],
-                    }}
-                    onChange={(value) => {
-                      setValue("content", value || "");
-                    }}
-                    minHeight={600}
-                  />
-                );
-              }}
-            />
-
-            {/* <textarea
-              className="border py-2 px-3 text-darkgrey-200 min-h-[400px]"
-              rows={8}
-              {...register("content")}
-            /> */}
+            <h2 className="mb-2xc py-2 font-title text-3xl text-blue-400 md:col-span-4">
+              Content
+            </h2>
+            <div className="min-h-[50vh]">
+              <Controller
+                control={control}
+                name="content"
+                render={({ field }) => {
+                  return (
+                    <MDEditor
+                      value={field.value}
+                      previewOptions={{
+                        rehypePlugins: [[rehypeSanitize]],
+                      }}
+                      onChange={(value) => {
+                        setValue("content", value || "");
+                      }}
+                      minHeight={600}
+                      height={800}
+                      preview="edit"
+                    />
+                  );
+                }}
+              />
+            </div>
           </div>
         </div>
         <div className="border-red text-red">
